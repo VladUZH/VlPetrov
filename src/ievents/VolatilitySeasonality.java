7ff6cb68346c8f01e70184d093ddb63d72323c80
Vladimir Petrov <vladimir.petrov@lykkex.com>
2017-05-23 10:38:33 +0200

    Corrected spelling: bin instead of bean)

--- src/ievents/VolatilitySeasonality.java
+++ src/ievents/VolatilitySeasonality.java
@@ -14,8 +14,8 @@ import org.joda.time.DateTimeConstants;
  * on the sum of the overshoot variability variability of overshoots (OS).
  *
  * The whole process consists of the following parts:
- *  1) to define the input parameters of the analysis, such like size of threshold and the length of one bean (for example, 10 min);
- *  2) to find sum of the variability of overshoots for each bean of a week;
+ *  1) to define the input parameters of the analysis, such like size of threshold and the length of one bin (for example, 10 min);
+ *  2) to find sum of the variability of overshoots for each bin of a week;
  *  3) to compute average value of the previous step
  *  4) to normalize the final value in such a way that the mean activity is equal to 1.0
  *  5) profit
@@ -23,23 +23,24 @@ import org.joda.time.DateTimeConstants;
 public class VolatilitySeasonality {
 
     private static final long MLS_WEAK = 604800000; // number of milliseconds in a week
-    private long timeOfBean; // defines the length (in milliseconds) of one bean
+    private long timeOfBin; // defines the length (in milliseconds) of one bin
     private DcOS dCoS; // an instance of the DcOS class which is used to compute all interested parameters.
-    private long nBeansInWeek; // how many beans we have in one week considering the chosen bean size.
-    private double[] activityList; // here we will store activity data for every bean.
-    private long timeFirstDC; // holds time of the first registered DC; will be used in the "computeTotalNumBeans"
-    private long timeLastDC; // holds time of the last registered DC; will be used in the "computeTotalNumBeans"
+    private long nBinsInWeek; // how many bins we have in one week considering the chosen bin size.
+    private double[] activityList; // here we will store activity data for every bin.
+    private long timeFirstDC; // holds time of the first registered DC; will be used in the "computeTotalNumBins"
+    private long timeLastDC; // holds time of the last registered DC; will be used in the "computeTotalNumBins"
+    private int[] dcCountList; // here we shall store total number of DC IEs at each bin
 
     /**
      *
      * @param threshold is size of the threshold used to find the number of DC and the variability of overshoots
-     * @param timeOfBean is length (in milliseconds) of one bean
+     * @param timeOfBin is length (in milliseconds) of one bin
      */
-    public VolatilitySeasonality(double threshold, long timeOfBean){
-        this.timeOfBean = timeOfBean;
+    public VolatilitySeasonality(double threshold, long timeOfBin){
+        this.timeOfBin = timeOfBin;
         dCoS = new DcOS(threshold, threshold, 1, threshold, threshold);
-        nBeansInWeek = MLS_WEAK / timeOfBean;
-        activityList = new double[(int) nBeansInWeek];
+        nBinsInWeek = MLS_WEAK / timeOfBin;
+        activityList = new double[(int) nBinsInWeek];
     }
 
     /**
@@ -54,18 +55,18 @@ public class VolatilitySeasonality {
             if (timeFirstDC == 0){
                 timeFirstDC = dcTime;
             }
-            int beanId = findBeanId(dcTime);
-            activityList[beanId] += dCoS.computeOSvariability();
+            int binId = findBinId(dcTime);
+            activityList[binId] += dCoS.computeOSvariability();
             timeLastDC = dcTime;
         }
     }
 
     /**
-     * This method compute the index of a bean in which the DC IE occurs.
+     * This method compute the index of a bin in which the DC IE occurs.
      * @param dcTime is the time of the observed DC IE.
-     * @return index (Id) ot the correspondent bean.
+     * @return index (Id) ot the correspondent bin.
      */
-    private int findBeanId(long dcTime){
+    private int findBinId(long dcTime){
         DateTime dcDateTime = new DateTime(dcTime);
         long mondayBeforeTime = dcDateTime.withDayOfWeek(DateTimeConstants.MONDAY).withTimeAtStartOfDay().getMillis();
         long millsFromMonday = dcTime - mondayBeforeTime - 1;
@@ -73,7 +74,7 @@ public class VolatilitySeasonality {
             System.out.println("ATTENTION! Winter time");
             millsFromMonday -= 3600000;
         }
-        return (int)(millsFromMonday / timeOfBean);
+        return (int)(millsFromMonday / timeOfBin);
     }
 
     /**
@@ -88,7 +89,7 @@ public class VolatilitySeasonality {
 
     /**
      *
-     * @return the total number of beans between the first and the last times of the registered DC IEs.
+     * @return the total number of bins between the first and the last times of the registered DC IEs.
      */
     private double computeTotalNumWeeks(){
         return (double)(timeLastDC - timeFirstDC) / MLS_WEAK;
@@ -111,7 +112,7 @@ public class VolatilitySeasonality {
 
     /**
      * The function simply divides all values in the activityList by the total number of full periods (weeks) in order
-     * to find the average activity of each bean.
+     * to find the average activity of each bin.
      */
     private void averageActivity(){
         double totalNumWeeks = computeTotalNumWeeks();
